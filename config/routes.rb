7958741f92963bb0130fc1c113c97d4eb536ecbe
gnguyen <gnguyen5464@gmail.com>
2019-07-17 13:50:56 -0400

    Add rails_admin gem and setup application name in the admin panel.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "stores#index"
-  
+
+  mount RailsAdmin::Engine => '/sp_admin', as: 'rails_admin'
   resources :stores
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
