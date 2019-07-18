3eb234b648cb24a13c232a60dee46125c123b1d1
gnguyen <gnguyen5464@gmail.com>
2019-07-18 13:18:40 -0400

    Config permit parameters to Devise control. Checked if current_user is admin then redirect to admin panel.

--- app/controllers/ims_controller.rb
+++ app/controllers/ims_controller.rb
@@ -1,7 +1,7 @@
 class ImsController < ApplicationController
   def index
-    if current_user
-      redirect_to stores_path
+    if current_user.try(:admin?)
+      redirect_to rails_admin_path
     end
   end
 end
